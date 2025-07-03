7165677c0865c33694bec42d1b60e983f4d4a921
Marcio Silva <marcio.mcbs@gmail.com>
2025-07-03 08:36:47 -0300

    Usar json na comunicação e metodo post

--- src/server.js
+++ src/server.js
@@ -4,11 +4,18 @@ const app = express();
 
 const PORT = 3333;
 
+app.use(express.json())
+
 app.get("/message/:name", (req, res) => {
   const { name } = req.params //Obrigatório para a rota
   const { page, limit = 10 } = req.query //Livre escolha para a rota
   res.send(`Hello, ${name}! Você está na página ${ page ? page : 1 } 
     com limite de ${limit}!`)
+})
+
+app.post("/users", (req, res) => {
+  const { name, email, password } = req.body;
+  res.json({ name, email, password })
 })
 
 app.listen(PORT, () => {
