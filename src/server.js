ae20b08e434f90cf0502224ee364e6d887cdcc0d
Marcio Silva <marcio.mcbs@gmail.com>
2025-07-03 08:24:17 -0300

    Uso do nodemon e rotas get: params e query (test)

--- src/server.js
+++ src/server.js
@@ -4,6 +4,13 @@ const app = express();
 
 const PORT = 3333;
 
+app.get("/message/:name", (req, res) => {
+  const { name } = req.params //Obrigatório para a rota
+  const { page, limit = 10 } = req.query //Livre escolha para a rota
+  res.send(`Hello, ${name}! Você está na página ${ page ? page : 1 } 
+    com limite de ${limit}!`)
+})
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 })
