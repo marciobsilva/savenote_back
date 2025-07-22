457322cb35ebaea4946c1cd6eefec9fbec24ca62
Marcio Silva <marcio.mcbs@gmail.com>
2025-07-22 11:58:43 -0300

    Agrupar tags por nome e instalei dotenv

--- src/controllers/TagsController.js
+++ src/controllers/TagsController.js
@@ -3,7 +3,7 @@ const knex = require("../database/knex")
 class TagsController {
     async index(req, res) {
         const user_id = req.user_id
-        const tags = await knex("tags").where({user_id})
+        const tags = await knex("tags").where({user_id}).groupBy("name")
 
         return res.json(tags)
     }
